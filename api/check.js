6c00d289e6d5b9c917136d092f3f9eabcfd5400a
ismadian69-hue <ismadian69@gmail.com>
2026-04-23 14:46:29 +0100

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,15 +1,16 @@
 export default async function handler(req, res) {
   const { ip } = req.query;
 
-  const response = await fetch(`https://api.abuseipdb.com/api/v2/check?ipAddress=${ip}`, {
-    method: "GET",
-    headers: {
-      "Key": process.env.API_KEY,
-      "Accept": "application/json"
+  const response = await fetch(
+    `https://api.abuseipdb.com/api/v2/check?ipAddress=${ip}`,
+    {
+      headers: {
+        "Key": process.env.API_KEY,
+        "Accept": "application/json"
+      }
     }
-  });
+  );
 
   const data = await response.json();
-
   res.status(200).json(data);
 }
